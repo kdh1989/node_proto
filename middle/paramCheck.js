4cfbee697c67414989fb6e186f05c7d898f0c191
darkdru1989 <darkdru1989@gmail.com>
2017-10-25 16:26:42 +0900

    Private 함수명 변경 _함수명, 함수 내 중복값 체크 함수 추가 ( isDuplicate )

--- middle/paramCheck.js
+++ middle/paramCheck.js
@@ -6,7 +6,7 @@ var CheckParam = function(param, CheckKey) {
 	var checkKey = CheckKey;
 
 	//키가 존재하는지 체크 
-	function isExits() {
+	function _isExits() {
 
 			var key = Object.keys(objParam).filter(function(value){
 				return value == checkKey;
@@ -22,13 +22,13 @@ var CheckParam = function(param, CheckKey) {
 	}
 
 	//키가 존재하는지 체크 
-	function PromiseisExits() {
+	function _PromiseisExits() {
 		return new Promise( function(resoleve, reject) {
 
 			if(objParam === null)
 				return reject("isExits Param ");
 
-			if(isExits())
+			if(_isExits())
 			{
 				return resoleve(true);
 			}
@@ -41,28 +41,38 @@ var CheckParam = function(param, CheckKey) {
 	}
 
 	//값이 있는지 체크 
-	function isEmpty() {
+	function _isEmpty() {
 
-	
-		if(objParam[checkKey] === null || objParam[checkKey] === "") { //값이 존재하는지 체크 
-			return false;
-		} else {
-			return true;
+		var isArray = typeof objParam[checkKey] === 'object' ? true : false; // 내부 값이 오브젝트 인지 체크 
+		
+		if(isArray) { //값이 오브젝트 형태이면 
+			if(objParam[checkKey].length) { //길이값으로 존재하는지 체크 
+				return true;
+			} else {
+				return false;
+			}
+		}
+		else { //값이 단일 형태이면 
+			if(objParam[checkKey] === null || objParam[checkKey] === "") { //값이 존재하는지 체크 
+				return false;
+			} else {
+				return true;
+			}
 		}
 
 
 	}
 
 	//값이 있는지 체크 
-	function PromiseisEmpty() {
+	function _PromiseisEmpty() {
 		return new Promise( function(resoleve, reject) {
 
 			if(objParam === null)
 				return reject("isExits Param ");
 
-			if(isExits()) { //키가 존재하는지 체크
+			if(_isExits()) { //키가 존재하는지 체크
 
-				if(isEmpty()) { //값이 존재하는지 체크 
+				if(_isEmpty()) { //값이 존재하는지 체크 
 					return resoleve(true);
 				} else {
 					return reject("isEmpty Key : "+checkKey);
@@ -75,9 +85,56 @@ var CheckParam = function(param, CheckKey) {
 
 	}
 
+	function _isDuplicate() {
+
+		var isArray = typeof objParam[checkKey] === 'object' ? true : false;
+
+		if(isArray) { //오브젝트만 중복 체크 가능
+
+			var uniq = objParam[checkKey].reduce(function(a,b) {
+				if(a.indexOf(b) < 0)
+					a.push(b);
+				return a;
+			},[]);
+
+			//console.log(uniq.length != objParam[checkKey].length);
+
+			if(uniq.length != objParam[checkKey].length) //중복 존재 
+				return false;
+			else
+				return true;
+
+		} else { //오브젝트가 아니기에 중복체크 못함
+			return false;
+		}
+
+	}
+
+	function _PromiseisDuplicate() {
+		return new Promise( function(resoleve, reject) {
+
+			if(objParam === null)
+				return reject("isExits Param ");
+
+			if(_isExits()) { //키가 존재하는지 체크
+
+				if(_isDuplicate()) { //값이 존재하는지 체크 
+					return resoleve(true);
+				} else {
+					return reject("isDuplicate Key : "+checkKey);
+				}
+
+			} else { //키가 존재하지 않는다.
+				return reject("isExits Key : "+checkKey);
+			}
+		});
+
+	}
+
 	return {
-		isExits:PromiseisExits,
-		isEmpty:PromiseisEmpty
+		isExits:_PromiseisExits,
+		isEmpty:_PromiseisEmpty,
+		isDuplicate:_PromiseisDuplicate
 	}
 
 }
